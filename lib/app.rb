f2750b5f10766894a65e3157be6a2876128690f2
Ari Kalfus <arikalfus@gmail.com>
2015-02-28 17:51:31 -0500

    Twitterwhack returning results

--- lib/app.rb
+++ lib/app.rb
@@ -1,29 +1,37 @@
 # noinspection RubyResolve
 require 'sinatra'
-require 'oauth'
 require 'json'
+require_relative 'simple_twitter'
 
 set :port, 3843
+enable :sessions
 
 # HTTP entry points
 get '/' do
   erb :name
 end
 
+get '/error' do
+  erb :error
+end
+
 post '/form' do
-  erb :form, :locals => { :name => params[:name] }
+  session[:name] = params[:name]
+  erb :form, :locals => { :name => session[:name] }
 end
 
-get '/results' do
-  erb :result
-end
-
-post '/calculate/whack' do
+post '/results' do
   first_word = params[:first]
   last_word = params[:second]
-  erb :calculating, :locals => { :name => params[:name] }
 
-  address = URI 'https://api.twitter.com/1.1/search/tweets.json'
+  first_result = Simple_Twitter.search first_word
+  second_result = Simple_Twitter.search last_word
+  whole_result = Simple_Twitter.search "#{first_word} #{last_word}"
 
+  redirect to '/error' if first_result.nil? || second_result.nil? || whole_result.nil?
+
+  erb :result, :locals => { :name => session[:name],
+                            :first_result => first_result,
+                            :second_result => second_result,
+                            :whole_result => whole_result }
 end
-
--- lib/simple_twitter.rb
+++ lib/simple_twitter.rb
@@ -1,12 +1,46 @@
 module Simple_Twitter
 
-  require_relative '../data/secrets'
+  require_relative '../data/secrets' # For consumer key and access token
+  require 'oauth'
+  require 'json'
+  require 'net/http' # Will also require 'uri'
 
-  consumer_key = OAuth::Consumer.new(
-      @con_key,
-      @con_secret)
-  access_token = OAuth::Token.new(
-      @acc_token,
-      @acc_secret)
+  $url = 'https://api.twitter.com'
+
+ def self.search(search_term)
+
+    address = URI "#{$url}/1.1/search/tweets.json?q=#{search_term}&count=100"
+
+    # Set up Net::HTTP to use SSL, which is required by Twitter.
+    http = Net::HTTP.new address.host, address.port
+    http.use_ssl = true
+    # noinspection RubyResolve
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+    # Build request and authorize it with OAuth
+    request = Net::HTTP::Get.new address.request_uri
+    request.oauth! http, @consumer_key, @access_token
+
+    # Issue request and return response.
+    http.start
+    response = http.request request
+
+    if response.code == '200'
+      JSON.parse(response.body)
+    else
+      nil
+    end
+
+  end
+
+  private
+  @consumer_key = OAuth::Consumer.new(
+      $con_key,
+      $con_secret)
+  @access_token = OAuth::Token.new(
+      $acc_token,
+      $acc_secret)
+
+  attr_reader :consumer_key, :access_token
 
 end
